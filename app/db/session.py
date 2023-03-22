494c498855b67eeff09724e031bcf09b93373f11
trickyrat <wangjian0513@outlook.com>
2023-03-22 14:28:34 +0800

    remove connection args to avoid connecting issue

--- app/db/session.py
+++ app/db/session.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}, future=True
+    settings.SQLALCHEMY_DATABASE_URI, future=True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
